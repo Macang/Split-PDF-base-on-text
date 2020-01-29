cc8a97a1e750cad5f57443cef8976282b43c8c80
Macang <macang@MacangdeMacBook-Pro.local>
2020-01-29 23:58:43 +0800

    main

--- pdfsplit.py
+++ pdfsplit.py
@@ -1,14 +1,15 @@
 import pdfplumber
 from PyPDF2 import PdfFileWriter,PdfFileReader
 import re
-object = PdfFileReader("*.pdf")
-with pdfplumber.open("*.pdf") as pdf:
+file = "printsample.pdf"    #*.pdf enter in your pdf file
+object = PdfFileReader(file)        
+with pdfplumber.open(file) as pdf:   
     start = 0
     pages = []
     final = [] 
     splitnum = []
     first_page = pdf.pages[0]
-    target = "your target text"
+    target = "6636"         #enter you want track text
     NumPages = object.getNumPages()
     end = []
     end.append(object.getNumPages())
@@ -19,16 +20,20 @@ with pdfplumber.open("*.pdf") as pdf:
         if type(ResSearch) == re.Match:
             splitnum.append(i)
     splitnum.extend(end)
-    splitnum.remove(0)
-    for point in splitnum:
-        list.clear(final)
-        output = PdfFileWriter()
-        for a in range (start,point):
-            pages.append(a)
-            final.extend(pages)
-            list.clear(pages)
-            start=point
-            newfile = str(point) + ".pdf"
-            output.addPage(object.getPage(a))
-            with open(newfile, "wb") as outputStream:
-                output.write(outputStream)
+    if 0 not in splitnum :
+        print("no text track")
+    else:
+        splitnum.remove(0)
+        for point in splitnum:
+            list.clear(final)
+            output = PdfFileWriter()
+            for a in range (start,point):
+                pages.append(a)
+                final.extend(pages)
+                list.clear(pages)
+                start=point
+                newfile = str(point) + ".pdf"
+                output.addPage(object.getPage(a))
+                with open(newfile, "wb") as outputStream:
+                    output.write(outputStream)
+        print("split done!")
